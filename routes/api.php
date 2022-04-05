3b40f01a5584b2f4832dfe5ca1466090c9203e12
APRISAL SYAM <815101162@intranet.pajak.go.id>
2022-04-05 14:42:29 +0700

    update dengan sanctum

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\DaftarsoalController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -14,9 +15,26 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+// Route::get('/soal', [DaftarsoalController::class, 'index']);
+// Route::get('/soal/{id}', [DaftarsoalController::class, 'show']);
 
-Route::resource('daftarsoals', DaftarsoalController::class);
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::get('/soal', [DaftarsoalController::class, 'index']);
+    Route::get('/soal/{id}', [DaftarsoalController::class, 'show']);
 });
+
+// Route::resource('daftarsoals', DaftarsoalController::class);
+
+// Route::post('login', [AuthController::class, 'signin']);
+// Route::post('register', [AuthController::class, 'signup']);
+
+// Route::middleware('auth:sanctum')->group( function () {
+//     Route::resource('daftarsoals', DaftarsoalController::class);
+// });
+
+// Route::middleware('auth:sanctum')->get('/user', function () {
+//     return $request->user();
+// });
